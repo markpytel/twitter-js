2a219930685027622fb5eade17f630699c6a56e5
Mark Pytel <markpytel@gmail.com>
2015-08-05 15:21:43 -0400

    using router to serve up css file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 var swig = require('swig');
+var tweetBank = require('./tweetBank');
+var routes = require('./routes/');
 swig.setDefaults({ cache: false });
 
 var port = 3000;
@@ -9,6 +11,9 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+app.use('/', routes);
+
+
 app.use(function (req, res, next) {
 	console.log('Response status code ' + res.statusCode);
 	console.log("Route: " + req.path + "\n" + "Method: "+ req.method);
@@ -16,18 +21,22 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/', function(req,res,next){
-	//res.send("Welcome\n");
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
+app.use(express.static(__dirname + '/public'));
 
 
-});
 
-app.get('/news', function(req,res,next){
-	res.send("Welcome to news\n");
+// app.get('/', function (req, res, next){
+// 	//res.send("Welcome\n");
+// 	var people = [{name: 'Full', lastname: "Name"}, {name: 'Stacker', lastname: 'Queuer'}, {name: 'Son', lastname: 'Daughter'}];
+// 	res.render( 'index', {title: 'Hall of Fame', people: people} );
 
-});
+
+// });
+
+// app.get('/news', function (req, res, next){
+// 	res.send("Welcome to news\n");
+
+// });
 
 app.listen(port, function (){
 	console.log('Awaiting order on port', port);
